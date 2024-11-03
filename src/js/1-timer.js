aba6be9e306823be18c34118df106584fd69a1cd
IvanGodPro24 <vanyanep@gmail.com>
2024-11-03 13:59:55 +0200

    Add homework

---
+++ src/js/1-timer.js
@@ -0,0 +1,140 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateInput = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('.btn-start');
+const spans = document.querySelectorAll('.value');
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() <= Date.now()) {
+      iziToast.error({
+        position: 'topRight',
+        iconUrl: '../img/error.svg',
+        backgroundColor: '#ef4040',
+
+        transitionIn: 'flipInX',
+        transitionOut: 'flipOutX',
+        title: 'Error',
+        titleColor: '#fff',
+        titleSize: '16',
+
+        message: `Please choose a date in the future`,
+        messageColor: '#fff',
+        messageSize: '16',
+
+        close: false,
+        buttons: [
+          [
+            '<button class="close-bnt"> <svg class="close-icon" width="16" height="16"><path d="M8 8l-4-4m4 4l4-4m-4 4l4 4m-4-4l-4 4" /></svg>  </button>',
+            function (instance, toast) {
+              instance.hide(
+                {
+                  transitionOut: 'fadeOutUp',
+                },
+                toast
+              );
+            },
+          ],
+        ],
+      });
+      btnStart.setAttribute('disabled', '');
+    } else {
+      userSelectedDate = selectedDates[0].getTime();
+
+      btnStart.removeAttribute('disabled');
+    }
+  },
+};
+
+flatpickr(dateInput, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function startTimer() {
+  const timerId = setInterval(() => {
+    dateInput.setAttribute('disabled', '');
+    btnStart.setAttribute('disabled', '');
+    const msDifference = userSelectedDate - Date.now();
+    const remainingTime = addLeadingZero(convertMs(msDifference));
+
+    spans[0].innerHTML = remainingTime.days;
+    spans[1].innerHTML = remainingTime.hours;
+    spans[2].innerHTML = remainingTime.minutes;
+    spans[3].innerHTML = remainingTime.seconds;
+
+    if (msDifference <= 0) {
+      clearInterval(timerId);
+
+      for (const span of spans) {
+        span.innerHTML = '00';
+      }
+
+      iziToast.info({
+        position: 'topRight',
+        iconUrl: '../img/ring.svg',
+        backgroundColor: '#09f',
+
+        transitionIn: 'flipInX',
+        transitionOut: 'flipOutX',
+        title: 'Info',
+        titleColor: '#fff',
+        titleSize: '16',
+
+        message: `Time is up!`,
+        messageColor: '#fff',
+        messageSize: '16',
+
+        close: false,
+        buttons: [
+          [
+            '<button class="close-bnt"> <svg class="close-icon" width="16" height="16"><path d="M8 8l-4-4m4 4l4-4m-4 4l4 4m-4-4l-4 4" /></svg>  </button>',
+            function (instance, toast) {
+              instance.hide(
+                {
+                  transitionOut: 'fadeOutUp',
+                },
+                toast
+              );
+            },
+          ],
+        ],
+      });
+
+      dateInput.removeAttribute('disabled');
+    }
+  }, 1000);
+}
+
+function addLeadingZero(object) {
+  for (let key in object) {
+    object[key] = String(object[key]).padStart(2, '0');
+  }
+
+  return object;
+}
+
+btnStart.addEventListener('click', startTimer);
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,133 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('.input-delay');
+const inputRadio = document.querySelectorAll('.input-radio');
+
+function generatePromise(event) {
+  event.preventDefault();
+
+  const delay = inputDelay.value;
+  let selectedRadioValue;
+
+  for (const radio of inputRadio) {
+    if (radio.checked) {
+      selectedRadioValue = radio.value;
+    }
+  }
+
+  form.reset();
+
+  if (delay < 0) {
+    iziToast.warning({
+      position: 'topRight',
+      iconUrl: '../img/warning.svg',
+      backgroundColor: '#ffa000',
+
+      transitionIn: 'flipInX',
+      transitionOut: 'flipOutX',
+      title: 'Warning',
+      titleColor: '#fff',
+      titleSize: '16',
+
+      message: 'Invalid value',
+      messageColor: '#fff',
+      messageSize: '16',
+
+      close: false,
+      buttons: [
+        [
+          '<button class="close-bnt"> <svg class="close-icon" width="16" height="16"><path d="M8 8l-4-4m4 4l4-4m-4 4l4 4m-4-4l-4 4" /></svg>  </button>',
+          function (instance, toast) {
+            instance.hide(
+              {
+                transitionOut: 'fadeOutUp',
+              },
+              toast
+            );
+          },
+        ],
+      ],
+    });
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedRadioValue === 'fulfilled') {
+        resolve(delay);
+      } else if (selectedRadioValue === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay =>
+      iziToast.success({
+        position: 'topRight',
+        iconUrl: '../img/success.svg',
+        backgroundColor: '#59a10d',
+
+        transitionIn: 'flipInX',
+        transitionOut: 'flipOutX',
+        title: 'Success',
+        titleColor: '#fff',
+        titleSize: '16',
+
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#fff',
+        messageSize: '16',
+
+        close: false,
+        buttons: [
+          [
+            '<button class="close-bnt"> <svg class="close-icon" width="16" height="16"><path d="M8 8l-4-4m4 4l4-4m-4 4l4 4m-4-4l-4 4" /></svg>  </button>',
+            function (instance, toast) {
+              instance.hide(
+                {
+                  transitionOut: 'fadeOutUp',
+                },
+                toast
+              );
+            },
+          ],
+        ],
+      })
+    )
+    .catch(delay =>
+      iziToast.error({
+        position: 'topRight',
+        iconUrl: '../img/error.svg',
+        backgroundColor: '#ef4040',
+
+        transitionIn: 'flipInX',
+        transitionOut: 'flipOutX',
+        title: 'Error',
+        titleColor: '#fff',
+        titleSize: '16',
+
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: '#fff',
+        messageSize: '16',
+
+        close: false,
+        buttons: [
+          [
+            '<button class="close-bnt"> <svg class="close-icon" width="16" height="16"><path d="M8 8l-4-4m4 4l4-4m-4 4l4 4m-4-4l-4 4" /></svg>  </button>',
+            function (instance, toast) {
+              instance.hide(
+                {
+                  transitionOut: 'fadeOutUp',
+                },
+                toast
+              );
+            },
+          ],
+        ],
+      })
+    );
+}
+
+form.addEventListener('submit', generatePromise);
